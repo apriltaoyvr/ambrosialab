e9107d0e7b598cd20c8a6852cd10fafd83934dfc
April Tao <95392008+apriltaoyvr@users.noreply.github.com>
2022-05-10 05:47:59 +0200

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,15 +1,6 @@
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-const { i18n } = require('./next-i18next.config');
-
-
 module.exports = {
-  nextConfig, 
   i18n: {
-    locales: ['en', 'it'],
     defaultLocale: 'it',
-    localePath: path.resolve('./public/static/locales')
-  }
-}
+    locales: ['en', 'it'],
+  },
+};
