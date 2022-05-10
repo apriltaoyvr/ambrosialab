9faea9c5016eb6ade1b1c3c9f2efa6b1abb595e5
April Tao <95392008+apriltaoyvr@users.noreply.github.com>
2022-05-09 22:50:16 -0700

    Locale

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
+    defaultLocale: 'it',
     locales: ['en', 'it'],
     localePath: path.resolve('./public/static/locales')
   }
