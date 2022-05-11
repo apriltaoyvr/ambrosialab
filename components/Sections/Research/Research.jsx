3a9788911050f6ddf25b3a74bb11fc60c138d218
April Tao <95392008+apriltaoyvr@users.noreply.github.com>
2022-05-10 18:15:14 -0700

    Revert "Column Adjustment"
    This reverts commit eb728da6c09703e63237e51dc2d6cc00122f0848.

--- components/Sections/Research/Research.jsx
+++ components/Sections/Research/Research.jsx
@@ -1,6 +1,22 @@
 import { useTranslation } from 'next-i18next';
-import { ItemWrapper, ResearchItem } from './ResearchItem';
+import styled from 'styled-components';
+import ResearchItem from './ResearchItem';
 import FadeIn from '../../FadeIn';
+
+const ItemWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5rem;
+
+  justify-content: center;
+  align-self: center;
+
+  @media screen and (min-width: 1200px) {
+    display: grid;
+    grid-template-columns: repeat(3, 300px);
+    grid-auto-columns: 500px;
+  }
+`;
 
 const Research = () => {
   const { t } = useTranslation('common');
--- components/Sections/Research/Research.styled.jsx
+++ components/Sections/Research/Research.styled.jsx
@@ -1,20 +1,5 @@
 import styled from 'styled-components';
 
-export const ItemWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 5rem;
-
-  justify-content: center;
-  align-self: center;
-
-  @media screen and (min-width: 1200px) {
-    display: grid;
-    grid-template-columns: repeat(3, 445px);
-    grid-auto-columns: 500px;
-  }
-`;
-
 export const ResearchCard = styled.div`
   display: flex;
   flex-direction: column;
