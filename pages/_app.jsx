ca9d5060a4199e016cf596059725472bc5a53b7b
April <95392008+apriltaoyvr@users.noreply.github.com>
2022-11-20 20:19:00 +0100

    Use next/font

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,7 @@
 import { appWithTranslation } from 'next-i18next';
+import { Fira_Code } from '@next/font/google';
+
+/* Styled Components */
 import GlobalStyle from '../components/utility/globalStyles';
 
 /* Font Awesome 
@@ -7,14 +10,15 @@ import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false;
 
+const fira = Fira_Code({ weight: '400', subsets: ['latin'] });
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} className={fira.className} />
     </>
-
-  )
+  );
 }
 
 export default appWithTranslation(MyApp);
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -39,17 +39,10 @@ class MyDocument extends Document {
             name='description'
             content='A health, wellness, and cosmetics research company headquartered in Ferrara, Italy.'
           />
-          <meta name='viewport' content='width=device-width, initial-scale=1' />
           <link rel='icon' type='image/svg' href='/static/images/logo.svg' />
           <link
             rel='image_src'
             href='https://user-images.githubusercontent.com/95392008/167490238-a75c7e8b-af98-4cbd-8a95-e94061497fad.png'
-          />
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Fira+Code&display=swap'
-            rel='stylesheet'
           />
         </Head>
         <body>
