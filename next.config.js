ed63eeab875a9d07708504eeaac28d02ab4a33af
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-09-04 04:26:14 -0700

    Misc

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
 const withNextIntl = require('next-intl/plugin')(
   // This is the default (also the `src` folder is supported out of the box)
-  './src/locales/i18n.ts'
+  './src/i18n.ts'
 );
 
 module.exports = withNextIntl({
--- src/app/[locale]/(sections)/research/ActivityCard/ActivityModal.tsx
+++ src/app/[locale]/(sections)/research/ActivityCard/ActivityModal.tsx
@@ -1,5 +1,6 @@
-'use client'
+'use client';
 import * as Dialog from '@radix-ui/react-dialog';
+import * as ScrollArea from '@radix-ui/react-scroll-area';
 import { Cross2Icon } from '@radix-ui/react-icons';
 
 export default function ActivityModal({
@@ -19,12 +20,12 @@ export default function ActivityModal({
         </button>
       </Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Overlay className='data-[state=open]:animate-overlayShow fixed inset-0 bg-viva-900/80' />
-        <Dialog.Content className='data-[state=open]:animate-contentShow fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] border border-viva-100 bg-viva-800 p-[25px] shadow-lg focus:outline-none'>
+        <Dialog.Overlay className='fixed inset-0 bg-viva-900/80 data-[state=open]:animate-overlayShow' />
+        <Dialog.Content className='fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] border border-viva-100 bg-viva-800 p-[25px] shadow-lg data-[state=open]:animate-contentShow focus:outline-none'>
           <Dialog.Title className='m-0 text-[17px] text-xl font-medium'>
             {title}
           </Dialog.Title>
-          <Dialog.Description className='mb-5 mt-[10px] whitespace-pre-line text-[15px] leading-normal'>
+          <Dialog.Description className='mb-5 mt-[10px] max-h-[400px] overflow-y-auto whitespace-pre-line text-[15px] leading-normal scrollbar-thin scrollbar-track-viva-700 scrollbar-thumb-viva-300 p-1'>
             {info}
           </Dialog.Description>
           <Dialog.Close asChild>
