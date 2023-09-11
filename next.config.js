58d7baa6f8081cca7183456d423b835452fccbd0
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-09-11 04:47:57 -0700

    Temporary change until migration

--- next.config.js
+++ next.config.js
@@ -5,4 +5,8 @@ module.exports = {
   compiler: {
     styledComponents: true,
   },
+  typescript: {
+    // Migrating to app directory. Best to do this in the meantime.
+    ignoreBuildErrors: true
+  }
 }
